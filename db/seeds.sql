832d517ce17eba7e98046a4624e331a1829fdc2b
Ryan Bregier <rabunc@gmail.com>
2017-09-04 22:39:47 -0400

    setup config and db

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("Jalepeño Burger", FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Cheddar Burger", FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Blue Cheese Burger", FALSE);
