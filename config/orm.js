58596aaaa2514aecff5ace154bc254f09b138dce
Ryan Bregier <rabunc@gmail.com>
2017-09-06 21:31:30 -0400

    made the orm abstract

--- config/orm.js
+++ config/orm.js
@@ -2,24 +2,26 @@ var connection = require("./connection.js");
 // ./ says this is a file on my computer, NOT an npm module
 
 var orm = {
-    selectAll: function(cb) {
-        connection.query("SELECT * FROM burgers;", function(err,data) {
+    selectAll: function(table, cb) {
+        connection.query(`SELECT * FROM ${table};`, function(err,data) {
             if (err) {
                 throw err;
             }
             cb(data)
         })
     },
-    insertOne: function(burger, cb) {
-        connection.query("INSERT INTO burgers (burger_name) VALUES (?);", [burger], function(err, result) {
+    insertOne: function(table, nameCol, burger, cb) {
+        // nameCol is burger_name
+        connection.query(`INSERT INTO ${table} (${nameCol}) VALUES (?);`, burger, function(err, result) {
             if (err) {
                 throw err;
             }
             cb(result)
         })
     },
-    updateOne: function(burger_id, cb) {
-        connection.query("UPDATE burgers SET devoured = 1 WHERE id = ?;", [burger_id], function(err, result) {
+    updateOne: function(table, burger_id, cb) {
+        console.log(burger_id)
+        connection.query(`UPDATE ${table} SET devoured = 1 WHERE id = ?;`, burger_id, function(err, result) {
             if (err) {
                 throw err;
             }
--- models/burger.js
+++ models/burger.js
@@ -2,17 +2,17 @@ var orm = require("../config/orm.js");
 
 var burger = {
     all: function(cb) {
-        orm.selectAll(function(res){
+        orm.selectAll("burgers", function(res){
             cb(res)
         })
     },
     create: function(burger, cb) {
-        orm.insertOne(burger, function(res){
+        orm.insertOne("burgers", "burger_name", burger, function(res){
             cb(res)
         })
     },
     update: function(burger, cb) {
-        orm.updateOne(burger, function(res) {
+        orm.updateOne("burgers", burger, function(res) {
             cb(res)
         })
     }
